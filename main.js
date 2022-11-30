7f08d405cf0a38e3ac36980514aaa0b439ca8a7a
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-11-30 11:04:23 +0400

    divContainer block

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+let button = document.querySelector('button');
+let divContainer = document.querySelector('.formDiv');
+let inputIt = document.querySelector('#prevent');
 
+button.addEventListener("click", function(event){
+    if(inputIt.value == ""){
+        event.preventDefault();
+        alert('Enter A Note');
+    }else{
+        divContainer.style.display = "block";
+        let input = document.querySelector('input');
+    }
+})
